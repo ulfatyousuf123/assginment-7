9d97aa22f88d1228731e0c5e31b1b109cada8793
ulfatyousuf <snow10515@gmail.com>
2023-10-10 09:27:10 -0700

    work done

---
+++ app.js
@@ -0,0 +1,45 @@
+
+// function message(){
+//     alert("HELLO PAKISTAN")
+// }
+
+
+
+// function login() {
+//       var name = document.getElementById("name");
+//        var email = document.getElementById("email");
+    
+//        if (name.value && email.value) {
+//          console.log(name.value);
+//          console.log(email.value);
+//        } else {
+//          alert("first fill the entire field..");
+//        }
+//      }
+
+// function value(){
+//     var num = document.getElementById("num");
+
+   
+  
+// }
+
+// function form(){
+//     var fname =document.getElementById("fname");
+//     var lname = document.getElementById("lname");
+//     var html =document.getElementById("html");
+//     var css = document.getElementById("css"); 
+//      var javascript = document.getElementById ("javascript");
+
+//     if (fname.value && lname.value && html.value && css.value && javascript.value ) {
+//         console.log(fname.value);
+//         console.log(lname.value);
+//         console.log(html.value);
+//         console.log(css.value);
+//         console.log(javascript.value);
+        
+
+//     } else{
+//         alert("first fill form...")
+//     }
+// }
